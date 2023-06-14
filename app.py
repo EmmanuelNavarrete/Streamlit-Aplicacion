46bf73e2ddf458c1c43445223041ba3aca28440a
EmmanuelNavarrete <s18030188@cdhidalgo.tecnm.mx>
2023-06-14 03:42:13 -0600

    f

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def main():
 	menu = ["Home","Recommend","Analisis de sentimientos","About",]
  
 	
-	logo = Image.open('img\logo.png')
+	logo = Image.open('img\osbe.png')
 	
 	st.sidebar.image(logo, width=300)
 	with st.sidebar.header("Menu"):
